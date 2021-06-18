82c9484a7f0d7f8da9bd481bf76ee89d6b0dbffe
developer.bami <developer.bami@gmail.com>
2021-06-18 12:58:41 +0300

    initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace SwitcherX
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtFrom = new System.Windows.Forms.TextBox();
+            this.txtTo = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnEncode = new System.Windows.Forms.Button();
+            this.btnHide = new System.Windows.Forms.Button();
+            this.chkClip = new System.Windows.Forms.CheckBox();
+            this.chkAuto = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(21, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Text to modify";
+            // 
+            // txtFrom
+            // 
+            this.txtFrom.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtFrom.Location = new System.Drawing.Point(21, 50);
+            this.txtFrom.Multiline = true;
+            this.txtFrom.Name = "txtFrom";
+            this.txtFrom.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtFrom.Size = new System.Drawing.Size(894, 122);
+            this.txtFrom.TabIndex = 1;
+            // 
+            // txtTo
+            // 
+            this.txtTo.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtTo.Location = new System.Drawing.Point(21, 227);
+            this.txtTo.Multiline = true;
+            this.txtTo.Name = "txtTo";
+            this.txtTo.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtTo.Size = new System.Drawing.Size(894, 268);
+            this.txtTo.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(21, 195);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Encoded text";
+            // 
+            // btnEncode
+            // 
+            this.btnEncode.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnEncode.Location = new System.Drawing.Point(527, 514);
+            this.btnEncode.Name = "btnEncode";
+            this.btnEncode.Size = new System.Drawing.Size(191, 39);
+            this.btnEncode.TabIndex = 4;
+            this.btnEncode.Text = "Encode";
+            this.btnEncode.UseVisualStyleBackColor = true;
+            this.btnEncode.Click += new System.EventHandler(this.btnEncode_Click);
+            // 
+            // btnHide
+            // 
+            this.btnHide.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnHide.Location = new System.Drawing.Point(724, 514);
+            this.btnHide.Name = "btnHide";
+            this.btnHide.Size = new System.Drawing.Size(191, 39);
+            this.btnHide.TabIndex = 5;
+            this.btnHide.Text = "Hide";
+            this.btnHide.UseVisualStyleBackColor = true;
+            this.btnHide.Click += new System.EventHandler(this.btnHide_Click);
+            // 
+            // chkClip
+            // 
+            this.chkClip.AutoSize = true;
+            this.chkClip.Location = new System.Drawing.Point(24, 518);
+            this.chkClip.Name = "chkClip";
+            this.chkClip.Size = new System.Drawing.Size(91, 17);
+            this.chkClip.TabIndex = 6;
+            this.chkClip.Text = "Use clipboard";
+            this.chkClip.UseVisualStyleBackColor = true;
+            // 
+            // chkAuto
+            // 
+            this.chkAuto.AutoSize = true;
+            this.chkAuto.Location = new System.Drawing.Point(148, 518);
+            this.chkAuto.Name = "chkAuto";
+            this.chkAuto.Size = new System.Drawing.Size(48, 17);
+            this.chkAuto.TabIndex = 7;
+            this.chkAuto.Text = "Auto";
+            this.chkAuto.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(948, 565);
+            this.ControlBox = false;
+            this.Controls.Add(this.chkAuto);
+            this.Controls.Add(this.chkClip);
+            this.Controls.Add(this.btnHide);
+            this.Controls.Add(this.btnEncode);
+            this.Controls.Add(this.txtTo);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtFrom);
+            this.Controls.Add(this.label1);
+            this.KeyPreview = true;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.ShowInTaskbar = false;
+            this.Text = "Switcher";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtFrom;
+        private System.Windows.Forms.TextBox txtTo;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnEncode;
+        private System.Windows.Forms.Button btnHide;
+        private System.Windows.Forms.CheckBox chkClip;
+        private System.Windows.Forms.CheckBox chkAuto;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SwitcherX
+{
+    public partial class Form1 : Form
+    {
+        KeyboardHook hook = new KeyboardHook();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+
+            setConst();
+
+            // register the event that is fired after the key press.
+            hook.KeyPressed +=
+                new EventHandler<KeyPressedEventArgs>(hook_KeyPressed);
+            // register the control + alt + F12 combination as hot key.
+            hook.RegisterHotKey(SwitcherX.ModifierKeys.Control | SwitcherX.ModifierKeys.Shift,
+                Keys.F12);
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        void hook_KeyPressed(object sender, KeyPressedEventArgs e)
+        {
+            // show the keys pressed in a label.
+            //this.Text = e.Modifier.ToString() + " + " + e.Key.ToString();
+
+            if (chkClip.Checked)
+            {
+                txtFrom.Text =Clipboard.GetText();
+                if (chkAuto.Checked)
+                {
+                    txtTo.Text = puntoSwitch(txtFrom.Text);
+                    Clipboard.SetText(txtTo.Text);
+                    this.WindowState = FormWindowState.Minimized;
+                    return;
+                }
+            }
+
+            this.WindowState = FormWindowState.Normal;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnHide_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Escape)
+            {
+                this.WindowState = FormWindowState.Minimized;
+            }
+        }
+
+
+        const string alphabitEn = "qwertyuiop[]\\asdfghjkl;'zxcvbnm,./`1234567890-=";
+        //const string alphabitEnShiftFrom = "QWERTYUIOP[]\\ASDFGHJKL;'ZXCVBNM,./`1234567890-=";
+        const string alphabitEnShiftTo = "QWERTYUIOP{}|ASDFGHJKL:\"ZXCVBNM<>?~!@#$%^&*()_+";
+        const string alphabitRu = "йцукенгшщзхъ\\фывапролджэячсмитьбю.ё1234567890-=";
+        const string alphabitRuShiftTo = "ЙЦУКЕНГШЩЗХЪ\\ФЫВАПРОЛДЖЭЯЧСМИТЬБЮ,Ё!\"№;%:?*()_+";
+
+
+        static List<String> list = new List<String>();
+   
+
+        static string puntoSwitch(string toDec)
+        {
+            string dec = "";
+            Boolean wayFound = false;
+            try
+            {
+                for (int i = 0; i < toDec.Length; i++)
+                {
+                    wayFound = false;
+
+                    for (int j = 0; j < list.Count; j++)
+                    {
+                        if (list[j][0] == toDec[i])
+                        {
+                            dec += list[j][1];
+                            wayFound = true;
+                            break;
+                        }
+                    }
+                    if (!wayFound)
+                    {
+                        for (int j = 0; j < list.Count; j++)
+                        {
+                            if (list[j][1] == toDec[i])
+                            {
+                                dec += list[j][0];
+                                wayFound = true;
+                                break;
+                            }
+                        }
+                    }
+
+                    if (!wayFound)
+                    {
+                        dec += toDec[i];
+                    }
+                }
+            }
+            catch (System.Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            return dec;
+        }
+
+        static void setConst()
+        {
+            list.Add("`ё");
+            list.Add("qй");
+            list.Add("wц");
+            list.Add("eу");
+            list.Add("rк");
+            list.Add("tе");
+            list.Add("yн");
+            list.Add("uг");
+            list.Add("iш");
+            list.Add("oщ");
+            list.Add("pз");
+            list.Add("[х");
+            list.Add("]ъ");
+            list.Add("aф");
+            list.Add("sы");
+            list.Add("dв");
+            list.Add("fа");
+            list.Add("gп");
+            list.Add("hр");
+            list.Add("jо");
+            list.Add("kл");
+            list.Add("lд");
+            list.Add(";ж");
+            list.Add("'э");
+            list.Add("\\\\");
+            list.Add("zя");
+            list.Add("xч");
+            list.Add("cс");
+            list.Add("vм");
+            list.Add("bи");
+            list.Add("nт");
+            list.Add("mь");
+            list.Add(",б");
+            list.Add(".ю");
+            list.Add("/.");
+            list.Add("~Ё");
+            list.Add("QЙ");
+            list.Add("WЦ");
+            list.Add("EУ");
+            list.Add("RК");
+            list.Add("TЕ");
+            list.Add("YН");
+            list.Add("UГ");
+            list.Add("IШ");
+            list.Add("Oщ");
+            list.Add("PЗ");
+            list.Add("{Х");
+            list.Add("}Ъ");
+            list.Add("AФ");
+            list.Add("SЫ");
+            list.Add("DВ");
+            list.Add("FА");
+            list.Add("GП");
+            list.Add("HР");
+            list.Add("JО");
+            list.Add("KЛ");
+            list.Add("LД");
+            list.Add(":Ж");
+            list.Add("\"Э");
+            list.Add("ZЯ");
+            list.Add("XЧ");
+            list.Add("CС");
+            list.Add("VМ");
+            list.Add("BИ");
+            list.Add("NТ");
+            list.Add("MЬ");
+            list.Add("<Б");
+            list.Add(">Ю");
+            list.Add("?,");
+            list.Add("#№");
+            list.Add("&?");
+        }
+
+        private void btnEncode_Click(object sender, EventArgs e)
+        {
+            txtTo.Text = puntoSwitch(txtFrom.Text);
+            if (chkClip.Checked)
+            {
+                Clipboard.SetText(txtTo.Text);
+            }
+        }
+    }
+}
